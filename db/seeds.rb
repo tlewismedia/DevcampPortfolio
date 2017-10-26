03b01de57b4b3028a489b052c01e502f42591aa6
Tom Lewis <tom@ricochetpartners.com>
2017-10-26 11:04:55 -0700

    implement defaults for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae, ipsa blanditiis sit officia quos fugiat quod recusandae, accusantium iste non illo vel. Facilis sint, ab nesciunt modi fugiat perferendis."
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima vitae, ipsa blanditiis sit officia quos fugiat quod recusandae, accusantium iste non illo vel. Facilis sint, ab nesciunt modi fugiat perferendis.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +23,21 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "my great service",
+    subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, temporibus, ipsa. Similique officia eum aut architecto excepturi impedit soluta neque iste accusantium consequatur, nobis",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
 end
-puts "9 portfolio items created"
-
+puts "8 portfolio items created"
+Portfolio.create!(
+  title: "Portfolio title: 9",
+  subtitle: "Angular",
+  body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, temporibus, ipsa. Similique officia eum aut architecto excepturi impedit soluta neque iste accusantium consequatur, nobis",
+  main_image: "http://placehold.it/600x400",
+  thumb_image: "http://placehold.it/350x200"
+)
+puts "1 portfolio items created"
