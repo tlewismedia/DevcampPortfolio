a659528d08cd7ff8555534fc8501a22265270294
Tom Lewis <tom@ricochetpartners.com>
2018-03-08 11:59:08 -0800

    implement authorization for portfolio and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,10 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
-  
+  access  all:        [:show, :index],
+          user:       {except: [:destroy, :new, :create, :update, :edit]},
+          site_admin: :all
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -30,7 +33,7 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body,
       technologies_attributes: [:name]))
@@ -47,7 +50,7 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    
+
 
     @portfolio_item.destroy
     respond_to do |format|
@@ -58,7 +61,7 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(  :title, 
+    params.require(:portfolio).permit(  :title,
                                         :subtitle,
                                         :body,
                                         technologies_attributes: [:name]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 	def login_helper
-		if current_user.is_a?(User) 
-		   link_to "Logout", destroy_user_session_path, method: :delete 
-		else 
+		if current_user.is_a?(GuestUser)
 		   (link_to "Register", new_user_registration_path) +
 		   "<br>".html_safe +
 		   (link_to "Login", new_user_session_path)
-		end 
+		else
+		   link_to "Logout", destroy_user_session_path, method: :delete
+		end
 	end
 
 	def sample_helper
 		content_tag :div, "My Content", class: "my-class"
-	end	
+	end
 
 	def copyright_generator
 		TlmViewTool::Renderer.copyright 'Tom Lewis', 'All Rights reserved'
