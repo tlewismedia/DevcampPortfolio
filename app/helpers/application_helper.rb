a1850e3bed502545236ff71dacf3689f85b43204
Tom <tlewismedia@gmail.com>
2018-05-03 21:40:55 -0700

    updated master styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,15 @@
 module ApplicationHelper
-	def login_helper
+	def login_helper style
 		if current_user.is_a?(User) 
-		   link_to "Logout", destroy_user_session_path, method: :delete 
+		   link_to "Logout", destroy_user_session_path, method: :delete, class: style 
 		else 
-		   (link_to "Register", new_user_registration_path) +
-		   "<br>".html_safe +
-		   (link_to "Login", new_user_session_path)
+		   (link_to "Register", new_user_registration_path, class: style ) +
+		   " ".html_safe +
+		   (link_to "Login", new_user_session_path, class: style )
 		end 
 	end
-
-	def sample_helper
-		content_tag :div, "My Content", class: "my-class"
-	end	
 
 	def copyright_generator
 		TlmViewTool::Renderer.copyright 'Tom Lewis', 'All Rights reserved'
 	end
-
 end
